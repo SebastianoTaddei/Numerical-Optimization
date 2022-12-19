bfe54be2c6da27f85f881998a39bfd4ad454642c
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2022-12-19 19:02:16 +0100

    Implemented a first version of Non-Linear Conjugate Gradient minimization.

--- matlab_code/exercise6/@NonLinearCGMinimization/NonLinearCGMinimization.m
+++ matlab_code/exercise6/@NonLinearCGMinimization/NonLinearCGMinimization.m
@@ -1,23 +1,48 @@
-classdef NonLinearCGMinimizationo
-    %NONLINEARCGMINIMIZATIONO Summary of this class goes here
-    %   Detailed explanation goes here
+classdef NonLinearCGMinimization < handle
+    %NONLINEARCGMINIMIZATION Class implementing non-linear Conjugate
+    %                        Gradient minization using the Fletcher and
+    %                        Reeves version
+    %   
+    %   Properties:
+    %    - max_iter -> maximum number of allowed iterations;
+    %    - tol      -> convergence tolerance;
+    %    - verbose  -> verbosity level:
+    %                   - false -> silent;
+    %                   - true  -> full report.
     
-    properties
-        Property1
+    properties (SetAccess = private, Hidden = true)
+        max_iter % maximum number of allowed iterations
+        tol      % convergence tolerance
+        verbose  % verbosity level
     end
     
     methods
-        function obj = NonLinearCGMinimizationo(inputArg1,inputArg2)
-            %NONLINEARCGMINIMIZATIONO Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
+        function obj = NonLinearCGMinimization()
+            %NONLINEARCGMINIMIZATION Construct an instance of this class
+            %   Sets the default values for the class properties
+            obj.max_iter = 1e2;
+            obj.tol      = 1e-8;
+            obj.verbose  = true;
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+
+        function set_max_iter(obj, max_iter)
+            %SET_MAX_ITER Set a new max_iter
+            obj.max_iter = max_iter;
         end
+
+        function set_tol(obj, tol)
+            %SET_TOL Set a new tol
+            obj.tol = tol;
+        end
+
+        function set_verbose(obj, verbose)
+            %SET_VERBOSE Set a new verbose
+            obj.verbose = verbose;
+        end
+
+        % Minimize using the Fletcher and Reeves non-linear Conjugate
+        % Gradient minimization
+        x = minimize(obj, fun, dfun, x0);
     end
 end
 
---
+++ matlab_code/exercise6/@NonLinearCGMinimization/minimize.m
@@ -0,0 +1,67 @@
+function x = minimize(obj, fun, dfun, x0)
+%MINIMIZE Using the Fletcher and Reeves non-linear Conjugate Gradient 
+%         minimization
+
+% Starting message
+if obj.verbose
+    fprintf('<strong>Starting minimization with Fletcher and Reeves NL CG.</strong>\n');
+end
+
+% Early check
+fx = feval(fun, x0);
+if abs(fx) <= obj.tol
+    if obj.verbose
+        fprintf('Minimum found at x0 = [%s].\n', join(string(x0), ', '));
+    end
+    
+    x = x0;
+
+    return;
+end
+
+% Initialise values
+x   = x0;
+dfx = feval(dfun, x0);
+p   = -dfx;
+
+% Intantiate linesearch object
+LS = LineSearch();
+LS.set_verbose(false);
+
+for i = 1:obj.max_iter
+    % Conjugate direction method
+    alpha    = LS.search(fun, dfun, fx, dfx, x, p);
+    x        = x + alpha*p;
+    dfx_next = feval(dfun, x);
+
+    % Report iteration
+    if obj.verbose
+        fprintf('%4d: x    = [%s];\n', i, join(string(x), '; '));
+        fprintf('      f(x) = %e.\n', fx);
+    end
+
+    % Convergence check
+    if norm(dfx_next) <= obj.tol
+        if obj.verbose
+            fprintf('<strong>Minimum found at x = [%s].</strong>\n', ...
+                join(string(x), '; '));
+        end
+
+        return;
+    end
+
+    % Residual orthogonalization
+    beta = (dfx_next'*dfx_next)/(dfx'*dfx);
+    p    = -dfx_next + beta*p;
+
+    % Update values
+    dfx = dfx_next;
+end
+
+% Maximum iterations reached
+if obj.verbose
+    fprintf('<strong>Maximum number of iterations reached.</strong>\n');
+end
+
+end
+
